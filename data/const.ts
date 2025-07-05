9e5714e1972887a67af2eeec235daacb2dd69b1b
Ilia Krasiuchenko <thesixteensaltines@gmail.com>
2025-07-05 13:23:38 +0700

    refactor: replace Map with Record for uniswapPoolNftContracts and WETHAddresses, streamline chain access methods

--- data/const.ts
+++ data/const.ts
@@ -2,41 +2,24 @@ import { Chain } from "viem"
 import { arbitrum } from "viem/chains"
 import { SupportedChains } from "./types"
 
-const uniswapPoolNftContracts: Map<SupportedChains, string> = new Map([
-  [SupportedChains.ArbitrumOne, "0xc36442b4a4522e871399cd717abdd847ab11fe88"],
-])
+const uniswapPoolNftContracts: Record<SupportedChains, string> = {
+  [SupportedChains.ArbitrumOne]: "0xc36442b4a4522e871399cd717abdd847ab11fe88", // V3
+}
 
 export const ONE_MONTH_S = 31 * 24 * 60 * 60 // 31 day in seconds
 export const ONE_HOUR_S = 60 * 60 // 1 hour in seconds
 export const ONE_MINUTE_S = 60 // 1 minute in seconds
 
-export const SupportedChainToChainMap: Map<SupportedChains, Chain> = new Map([
-  [SupportedChains.ArbitrumOne, arbitrum],
-])
-
-/**
- * Block time in seconds
- */
-const BlockTimes: Map<SupportedChains, number> = new Map([
-  [SupportedChains.ArbitrumOne, 0.26],
-])
-
-const WETHAddresses: Map<SupportedChains, string> = new Map([
-  [SupportedChains.ArbitrumOne, "0x82af49447d8a07e3bd95bd0d56f35241523fbab1"],
-])
-
-export function getChain(chain: SupportedChains) {
-  const occurrence = SupportedChainToChainMap.get(chain)
-
-  if (!occurrence) {
-    throw new Error(`Chain ${chain} is not supported`)
-  }
-
-  return occurrence
+export const ChainDictionary: Record<SupportedChains, Chain> = {
+  [SupportedChains.ArbitrumOne]: arbitrum,
 }
 
-export function getBlockTime(chain: SupportedChains) {
-  const occurrence = BlockTimes.get(chain)
+const WETHAddresses: Record<SupportedChains, string> = {
+  [SupportedChains.ArbitrumOne]: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+}
+
+export function getChain(chain: SupportedChains) {
+  const occurrence = ChainDictionary[chain]
 
   if (!occurrence) {
     throw new Error(`Chain ${chain} is not supported`)
@@ -46,7 +29,7 @@ export function getBlockTime(chain: SupportedChains) {
 }
 
 export function getUniswapPoolNftContract(chain: SupportedChains) {
-  const occurrence = uniswapPoolNftContracts.get(chain)
+  const occurrence = uniswapPoolNftContracts[chain]
 
   if (!occurrence) {
     throw new Error(`Chain ${chain} is not supported`)
@@ -56,7 +39,7 @@ export function getUniswapPoolNftContract(chain: SupportedChains) {
 }
 
 export function getWETHAddress(chain: SupportedChains) {
-  const occurrence = WETHAddresses.get(chain)
+  const occurrence = WETHAddresses[chain]
 
   if (!occurrence) {
     throw new Error(`Chain ${chain} is not supported`)
